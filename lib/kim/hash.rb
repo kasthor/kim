939581645bf6fea84753fb2b444597a30825d64e
Giancarlo Palavicini <kasthor@kasthor.com>
2011-08-18 11:51:58 -0600

    Fixed bug incorrect reference to self

--- lib/kim/hash.rb
+++ lib/kim/hash.rb
@@ -4,9 +4,9 @@ module Kim
       merge_method = bang ? :merge! : :merge
       other.inject(self) do | result, item |
         key, value = item
-        self.__send__ merge_method, (
-          if has_key? key and self[key].is_a? Hash and value.is_a? Hash
-            { key => self[key].deep_merge(value) }
+        result.__send__ merge_method, (
+          if has_key? key and result[key].is_a? Hash and value.is_a? Hash
+            { key => result[key].deep_merge(value) }
           else
             other
           end 
